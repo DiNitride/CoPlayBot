c66cd3b4470bf8d166069e8b94ba2727b3ad494c
DiNitride <jamesthecookie@gmail.com>
2016-06-23 11:07:46 +0100

    Adding Bot

---
+++ bot.py
@@ -0,0 +1,62 @@
+#
+#
+# CoPlay Cafe Server Bot
+# Written and updated by DiNitride
+#
+#
+
+# Importing things
+import discord
+from discord.ext import commands
+from utils import checks
+
+# Set's bot's desciption and prefixes in a list
+description = "A bot for the CoPlay Cafe Community Server"
+bot = commands.Bot(command_prefix=['?','coplay/'], description=description)
+
+#################
+##Startup Stuff##
+#################
+
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    # Outputs login data to console
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    await bot.change_status(discord.Game(name="!join / !leave"))
+
+    # Outputs the state of loading the modules to the console
+    # So I know they have loaded correctly
+    bot.load_extension("modules.subscriptions")
+    print("Loaded Subscriptions")
+
+####################
+##Misc and Testing##
+####################
+
+# Command to update the bot's profile picture
+@bot.command(hidden=True)
+@checks.is_admin()
+async def updateimage():
+    #Loads and sets the bot's profile image
+    with open("logo.jpg","rb") as logo:
+        await bot.edit_profile(avatar=logo.read())
+
+# Ping Pong
+# Testing the response of the bot
+@bot.command(hidden=True)
+async def ping():
+    """Pong"""
+    await bot.say("Pong")
+    print("Ping Pong")
+
+############################
+##FANCY TOKEN LOGIN STUFFS##
+############################
+
+with open("token.txt","r") as token:
+    bot.run(token.read())
---
+++ modules/subscriptions.py
@@ -0,0 +1,121 @@
+import discord
+from discord.ext import commands
+from utils import checks
+import asyncio
+
+class Subscriptions():
+    def __init__(self, bot):
+        self.bot = bot
+
+        # Message delete delay
+        self.delay = 10
+
+        # Options for different gams
+        # ID first entry
+        # Pretty, correctly spelt name as last
+        # Various ways of inputting in the middle
+        self.csgo = ['195471958534651904','csgo','counter strike','Counter Strike']
+        self.overwatch = ['195471986808455168','ow','overwatch','Overwatch']
+        self.lol = ['195472348420243456','lol','league','League Of Legends']
+        self.paragon = ['195472489642590208','paragon','Paragon']
+        self.pokemon = ['195472571817263105','pkmn','pokemon','Pokemon']
+        self.rpg = ['195472607900860416','rpg','RPG']
+        self.smash = ['195472623487025154','smash','Smash']
+        self.dank = ['195473816850268160','dank','Dank/Shitposting']
+        self.music = ['195473785447514112','music','Music']
+
+        self.allOptions = [self.csgo, self.overwatch, self.lol, self.paragon, self.pokemon, self.rpg, self.smash, self.dank, self.music]
+
+        # Creates a string of availible games to Subscribe to
+        listStr = ""
+        for x in self.allOptions:
+            listStr = listStr + ("\n" + x[-1])
+
+        # Makes that string look pretty
+        self.roles = "```Channels available to subscribe to: %s ```" % listStr
+
+    # Joins the user to a role
+    @checks.is_server()
+    @commands.command(hidden=True,pass_context=True)
+    async def join(self, ctx, *, channel: str):
+        """Adds you to a regions role"""
+
+        # Get's the server and user from message
+        server = ctx.message.server
+        member = ctx.message.author
+
+        # Sets the region in invalid
+        valid = False
+
+        # Defines whether this command is adding or
+        # removing role
+        self.sub = True
+
+        # Checks if the region is an available option
+        for options in self.allOptions:
+            if channel.lower() in options:
+                # Gets the role object from Discord
+                role = discord.utils.get(server.roles, id=options[0])
+                # Adds the role
+                await self.bot.add_roles(member, role)
+                # Outputs conformation to user
+                output = await self.bot.say("%s joined %s" % (member.mention, options[-1]))
+                await asyncio.sleep(self.delay)
+                await self.bot.delete_message(ctx.message)
+                await self.bot.delete_message(output)
+                # Outputs to console
+                print("%s joined %s" % (member.name, options[-1]))
+                #Ensures valid is True
+                valid = True
+                break
+
+        # If region is not valid, output available roles
+        if not valid:
+            output = await self.bot.say(self.roles)
+            await asyncio.sleep(self.delay)
+            await self.bot.delete_message(ctx.message)
+            await self.bot.delete_message(output)
+
+    @checks.is_server()
+    @commands.command(hidden=True,pass_context=True)
+    async def leave(self, ctx, *, channel: str):
+        """Removes you from a regions role"""
+
+        # Get's the server and user from message
+        server = ctx.message.server
+        member = ctx.message.author
+
+        # Sets the region in invalid
+        valid = False
+
+        # Defines whether this command is adding or
+        # removing role
+        self.sub = True
+
+        # Checks if the region is an available option
+        for options in self.allOptions:
+            if channel.lower() in options:
+                # Gets the role object from Discord
+                role = discord.utils.get(server.roles, id=options[0])
+                # Removes the role
+                await self.bot.remove_roles(member, role)
+                # Outputs conformation to user
+                output = await self.bot.say("%s left %s" % (member.mention, options[-1]))
+                await asyncio.sleep(self.delay)
+                await self.bot.delete_message(ctx.message)
+                await self.bot.delete_message(output)
+                # Outputs to console
+                print("%s left %s" % (member.name, options[-1]))
+                # Ensures valid is True
+                valid = True
+                break
+
+        # If region is not valid, output available roles
+        if not valid:
+            output = await self.bot.say(self.roles)
+            await asyncio.sleep(self.delay)
+            await self.bot.delete_message(ctx.message)
+            await self.bot.delete_message(output)
+
+def setup(bot):
+    bot.add_cog(Subscriptions(bot))
